786ec5eaa25fce7636db4408c1422704bb25842e
Karim90403 <karim90403@gmail.com>
2021-11-21 01:03:22 +0300

    1st version

---
+++ Agrohack/agrohack.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	$('.background').hide();
+	$('.file_title').hide();
+	$('.download_files').hide();
+	
+});
+
+$('.route').click(function() {
+		let form = $('form');
+		$.ajax({
+			url: form.attr('action'),
+			method: 'POST',
+			data: form.serialize(),
+			success: function(res) {
+				$('.background').show();
+				$.ajax({
+				    url: '/result?id='+res,
+					method: 'GET',
+				    success: function(res) {
+					    $('.background').hide();
+					    $('.file_title').show();
+					    $('.download_files').show();
+						$('.download_file').html(res);
+					},
+					complete: function(xhr, msg) {
+    					if (xhr.status != 200) alert(xhr.responseText || msg);
+					}
+				});
+			},
+			complete: function(xhr, msg) {
+				if (xhr.status != 200) alert(xhr.responseText || msg);
+			}
+		});
+	});
+
+
+
